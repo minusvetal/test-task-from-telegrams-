75bd05f389ca747b8b01c95fbd72961bfeb755c6
minusvetal <minusvetal@gmail.com>
2021-08-03 13:03:42 +0300

    add swiper slider

---
+++ app/js/app.js
@@ -0,0 +1,49 @@
+//Make sure that the dom is ready
+$(function () {
+	$(".star").rateYo({
+		starWidth: "16px",
+		normalFill: "#CACACA",
+		ratedFill: "#F7AF08",
+		readOnly: true,
+	});
+});
+
+// swiper
+new Swiper(".about-slider", {
+	// стрелки управления
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	// пагинация внизу
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction",
+		renderFraction: function (currentClass, totalClass) {
+			return (
+				'Отзыв <span class=" ' +
+				currentClass +
+				'"></span>' +
+				" из " +
+				'<span class="' +
+				totalClass +
+				'" ></span>'
+			);
+		},
+	},
+	// управление стрелками клавиатуры
+	keyboard: {
+		enabled: true,
+		pageUpDown: true,
+		onlyInViewport: true,
+	},
+	// управление колесиком мышки
+	mousewheel: {
+		sensitivity: 1,
+		eventsTarget: ".about-carusel__item",
+	},
+	// автовысота по размеру слайда
+	autoHeight: false,
+	// количество слайдов
+	slidesPerView: 3,
+});
