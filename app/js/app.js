9ccdedcf80f161d63d087639f669a0e6c8b8f5a2
minusvetal <minusvetal@gmail.com>
2021-08-04 14:43:56 +0300

    add footer + page transitions

--- app/js/app.js
+++ app/js/app.js
@@ -9,28 +9,28 @@ $(function () {
 });
 
 // swiper
-new Swiper(".about-slider", {
+new Swiper(".feedback-slider", {
 	// стрелки управления
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
 	// пагинация внизу
-	pagination: {
-		el: ".swiper-pagination",
-		type: "fraction",
-		renderFraction: function (currentClass, totalClass) {
-			return (
-				'Отзыв <span class=" ' +
-				currentClass +
-				'"></span>' +
-				" из " +
-				'<span class="' +
-				totalClass +
-				'" ></span>'
-			);
-		},
-	},
+	// pagination: {
+	// 	el: ".swiper-pagination",
+	// 	type: "fraction",
+	// 	renderFraction: function (currentClass, totalClass) {
+	// 		return (
+	// 			'Отзыв <span class=" ' +
+	// 			currentClass +
+	// 			'"></span>' +
+	// 			" из " +
+	// 			'<span class="' +
+	// 			totalClass +
+	// 			'" ></span>'
+	// 		);
+	// 	},
+	// },
 	// управление стрелками клавиатуры
 	keyboard: {
 		enabled: true,
@@ -46,4 +46,31 @@ new Swiper(".about-slider", {
 	autoHeight: false,
 	// количество слайдов
 	slidesPerView: 3,
+});
+
+// up
+
+$(function () {
+	// при нажатии на кнопку scrollup
+	$(".scrollup").click(function () {
+		// переместиться в верхнюю часть страницы
+		$("html, body").animate(
+			{
+				scrollTop: 0,
+			},
+			500,
+		);
+	});
+});
+// при прокрутке окна (window)
+$(window).scroll(function () {
+	// если пользователь прокрутил страницу более чем на 200px
+	if ($(this).scrollTop() > 200) {
+		// то сделать кнопку scrollup видимой
+		$(".scrollup").fadeIn();
+	}
+	// иначе скрыть кнопку scrollup
+	else {
+		$(".scrollup").fadeOut();
+	}
 });
